7b5718dd45b11d7e333f7350ebd34a64d6b01464
Zen T(halen) A(beysiri) Gunawardhana <38821230+zentag@users.noreply.github.com>
2025-12-07 14:13:40 -0500

    make joysticks usabel

--- genered/src/main/java/frc/robot/RobotContainer.java
+++ genered/src/main/java/frc/robot/RobotContainer.java
@@ -46,9 +46,9 @@ public class RobotContainer {
         drivetrain.setDefaultCommand(
             // Drivetrain will execute this command periodically
             drivetrain.applyRequest(() ->
-                drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-                    .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                    .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+                drive.withVelocityX(joystick.button(5).getAsBoolean() ?  -joystick.getRawAxis(1) * MaxSpeed : -joystick.getRawAxis(1) * MaxSpeed * .3) // Drive forward with negative Y (forward)
+                    .withVelocityY(joystick.button(5).getAsBoolean() ? -joystick.getRawAxis(0) * MaxSpeed  : -joystick.getRawAxis(0) * MaxSpeed * .3) // Drive left with negative X (left)
+                    .withRotationalRate(-joystick.getRawAxis(2) * MaxAngularRate) // Drive counterclockwise with negative X (left)
             )
         );
 
