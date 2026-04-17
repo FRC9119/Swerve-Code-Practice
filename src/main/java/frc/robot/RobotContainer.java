a567ecb90385dfdd22bb01c44e9167c0c2b660da
Zen T(halen) A(beysiri) Gunawardhana <38821230+zentag@users.noreply.github.com>
2026-04-17 13:14:28 -0400

    negate the joystick for passing (its unintuitive)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -131,7 +131,7 @@ public class RobotContainer {
                 operatorController.a()
                                 .whileTrue(ballSubsystem.unclogCommand());
                 operatorController.rightTrigger()
-                                .whileTrue(ballSubsystem.launchWithoutTargeting(operatorController.getRightY() * 20 + 65));
+                                .whileTrue(ballSubsystem.launchWithoutTargeting(-operatorController.getRightY() * 20 + 65));
                 // Start and end SysId logging
                 operatorController.leftBumper().onTrue(Commands.runOnce(SignalLogger::start));
                 operatorController.rightBumper().onTrue(Commands.runOnce(SignalLogger::stop));
